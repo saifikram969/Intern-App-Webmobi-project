5ba5fd5537ec7c1c396ce063a16c39a6fc780a69
saif ikram <saifikram969@gmail.com>
2023-12-12 23:00:42 +0530

    project update for github

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "intern App-Webmobi-Project"
+include(":app")
+ 
