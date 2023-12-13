75f4d8042be435a5003e57d5e010a48ce2ba954f
saif ikram <saifikram969@gmail.com>
2023-12-13 19:08:09 +0530

    test

--- app/src/main/java/com/example/internapp_webmobi_project/Edit_Profile.java
+++ app/src/main/java/com/example/internapp_webmobi_project/Edit_Profile.java
@@ -10,5 +10,6 @@ public class Edit_Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+        //bhai update hua code dekh or ss send kr meko
     }
 }
