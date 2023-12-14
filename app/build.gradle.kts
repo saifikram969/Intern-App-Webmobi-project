b5b285496f145270c6ab06cab25a509d9da1ef48
VicParihar <vivekparihar65@gmail.com>
2023-12-14 18:07:11 +0530

    updating in create_account_activity and creating editprofile_activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -41,8 +40,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("de.rhododendron:circumnavigate:3.1.0")
-    androidTestImplementation("com.square.picasso:picasso:2.71828")
-    androidTestImplementation("'com.github.bumptech.glide:compiler:4.12.0'")
-
+    androidTestImplementation("de.hdodenhof:circleimageview:3.1.0")
+    androidTestImplementation("com.squareup.picasso:picasso:2.71828")
+    androidTestImplementation("com.github.bumptech.glide:compiler:4.12.0")
 }
