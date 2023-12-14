fccffcf4c05b29eac2d5219f82b805ba5aca52f5
VicParihar <vivekparihar65@gmail.com>
2023-12-14 18:16:49 +0530

    updating in create_account_activity and creating editprofile_activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,10 +33,13 @@ android {
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    //noinspection GradleCompatible
+    androidTestImplementation("android.appcompat:appcompat:1.6.1")
+
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth:22.3.0")
+
     testImplementation("junit:junit:4.13.2")
     implementation("androidx.test.ext:junit:1.1.5")
     implementation("androidx.test.espresso:espresso-core:3.5.1")
